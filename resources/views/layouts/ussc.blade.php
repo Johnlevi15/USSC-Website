c4a3b391c5a51995bd4ee53e8393b0bce2b35086
Johnlevi15 <johnleviolpindo15@gmail.com>
2026-09-25 14:24:57 +0800

    burger nav for mobile

--- resources/views/layouts/ussc.blade.php
+++ resources/views/layouts/ussc.blade.php
@@ -26,7 +26,7 @@
         <button
             id="mobile-menu-open"
             type="button"
-            class="inline-flex h-10 w-10 items-center justify-center rounded-lg border border-red-700 bg-red-950 text-white transition hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-yellow-400 md:hidden"
+            class="inline-flex h-10 w-10 items-center justify-center rounded-lg bg-red-950 text-white transition hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-yellow-400 md:hidden"
             aria-controls="mobile-menu-panel"
             aria-expanded="false"
             aria-label="Open navigation menu"
